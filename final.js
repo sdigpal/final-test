021a8d1e48c29f6b0ca73d9a61670be09bd52141
sdigpal <sdigpal@myseneca.ca>
2022-12-08 15:21:09 -0500

    initial commit

--- final.js
+++ final.js
@@ -1,11 +1,26 @@
-const Sequelize = require('sequelize');
-var sequelize = new Sequelize('berxvwgt', 'berxvwgt', '31i6hIv2JGXtNytTiW83bV_prG7PERs1', {
-    host: 'https://rich-erin-outerwear.cyclic.app',
-    dialect: 'postgres',
-    port: 5432,
-    dialectOptions: {
-        ssl: { rejectUnauthorized: false }
-    },
-    query: { raw: true }
+var moongoose= require("mongoose");
+const bcrypt = require('bcryptjs');
+var Schema = mongoose.Schema;
+mongoose.connect("Your connection string here");
+var Schema = new Schema({
+  "Email":{
+    "type": string,
+    "unique": true} ,
+  "password": String
 });
+
+let finalUsers;
+module.exports.startDB = function () {
+    return new Promise(function (resolve, reject) {
+        const db= mongoose.createconnection("mongodb+srv://senecaweb.rsbwdc7.mongodb.net/myFirstDatabase", {useNewUrlParser: true});
+        db.on('error', function(err){
+            console.log("Cannot connect to DB");    
+        });
+        db.once('open', function(){
+            User = mongoose.model("finalUsers", new_Schema);
+            console.log("DB connection successful.");
+            resolve();
+         });
+     });
+ };
 
